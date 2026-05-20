14221dca0d48c4a9db2f28b019c980ece5d94889
naufalbintang <naufalbintangpradana@gmail.com>
2026-05-21 00:32:49 +0700

    fixing relationship

--- backend/models/historySkill.js
+++ backend/models/historySkill.js
@@ -4,11 +4,23 @@ const sequelize = require("../config/database");
 const HistorySkill = sequelize.define(
   "HistorySkill",
   {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    status: {type: DataTypes.ENUM("match", "gap"), allowNull: false}
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id_history: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "Histories", key: "id" },
+    },
+    id_skill: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "Skills", key: "id" },
+    },
+    status: { type: DataTypes.ENUM("match", "gap"), allowNull: false },
   },
   {
     tableName: "History_Skills",
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
+
+module.exports = HistorySkill;
