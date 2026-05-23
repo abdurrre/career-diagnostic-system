acad8f4c01b59e6bf80257c1a5401f84ea74d988
naufalbintang <naufalbintangpradana@gmail.com>
2026-05-23 16:09:28 +0700

    menambahkan kolom category pada model historySkill.js

--- backend/models/historySkill.js
+++ backend/models/historySkill.js
@@ -16,6 +16,10 @@ const HistorySkill = sequelize.define(
       references: { model: "Skills", key: "id" },
     },
     status: { type: DataTypes.ENUM("match", "gap"), allowNull: false },
+    category: {
+      type: DataTypes.ENUM("critical", "important", "supplementary"),
+      allowNull: true
+    },
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,
