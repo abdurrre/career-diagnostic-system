c91827952c68db8e594319aeb7c0691c47bc872e
naufalbintang <naufalbintangpradana@gmail.com>
2026-05-29 16:43:12 +0700

    security: ubah pesan error masuk menjadi "Email atau password salah" demi mencegah pelacakan akun

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,10 +36,12 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
-    if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
+    if (!user)
+      return res.status(401).json({ message: "Email atau password salah" });
 
     const validPass = await bcrypt.compare(password, user.password);
-    if (!validPass) return res.status(400).json({ message: "Password salah" });
+    if (!validPass)
+      return res.status(401).json({ message: "Email atau password salah" });
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
